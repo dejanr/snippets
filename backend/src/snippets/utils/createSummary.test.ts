01697d92ded86cfd555f134540aa470187501409
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-06-01 15:17:27 +0200

    Mock openai call with vitest-fetch-mock

--- backend/src/snippets/utils/createSummary.test.ts
+++ backend/src/snippets/utils/createSummary.test.ts
@@ -1,8 +1,29 @@
-import { expect, test } from 'vitest'
+import { vi, expect, test, beforeEach, afterEach } from 'vitest'
 import { createSummary } from './createSummary'
+import createFetchMock from 'vitest-fetch-mock';
+
+const fetchMocker = createFetchMock(vi);
+
+beforeEach(() => {
+  fetchMocker.enableMocks();
+});
 
 test('it should call openai and return summary of text', async () => {
+  fetchMocker.mockResponseOnce(JSON.stringify({
+    choices: [
+      {
+        message: {
+          content: 'A JavaScript statement that logs “Hello World!” to the console.'
+        }
+      }
+    ]
+  }));
+
   const summary = await createSummary('console.log("Hello World!")')
 
   expect(summary).toStrictEqual('A JavaScript statement that logs “Hello World!” to the console.')
 })
+
+afterEach(() => {
+  fetchMocker.disableMocks();
+});
