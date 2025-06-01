ec2808da4cc6e78af341c8e62bcd235e4f7ec4dc
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-06-01 15:31:26 +0200

    Use createSummary when creating new snippet

--- backend/src/snippets/create.test.ts
+++ backend/src/snippets/create.test.ts
@@ -1,5 +1,14 @@
-import { expect, test } from 'vitest'
+import { vi, beforeEach, expect, test } from 'vitest'
 import snippets from './index'
+import { createSummary } from './utils/createSummary'
+
+vi.mock('./utils/createSummary', () => ({
+  createSummary: vi.fn()
+}))
+
+beforeEach(() => {
+  vi.clearAllMocks()
+})
 
 test('Creating a new snippet should return status 201', async () => {
   const res = await snippets.request('/', {
@@ -8,4 +17,16 @@ test('Creating a new snippet should return status 201', async () => {
   })
 
   expect(res.status).toBe(201)
+})
+
+test('Creating a new snippet should populate summary from openai', async () => {
+  vi.mocked(createSummary).mockResolvedValue('Prints Hello World')
+
+  const res = await snippets.request('/', {
+    method: 'POST',
+    body: JSON.stringify({ text: 'console.log("Hello World")' }),
+  })
+  const response = await res.json()
+
+  expect(response.summary).toBe('Prints Hello World')
 })
--- backend/src/snippets/create.ts
+++ backend/src/snippets/create.ts
@@ -1,14 +1,16 @@
-import { randomUUID } from 'crypto';
 import type { Context } from 'hono'
+import type { Snippet } from "@snippets/shared"
+import { randomUUID } from 'crypto';
 import mongoose from 'mongoose'
 import { SnippetModel } from './db/model'
+import { createSummary } from './utils/createSummary';
 
 export async function createHandler(c: Context) {
   const { text } = await c.req.json()
   const snippet = new SnippetModel({
     id: randomUUID(),
     text,
-    summary: ''
+    summary: await createSummary(text)
   })
 
   await mongoose.connect(process.env.MONGODB_URI as string)
@@ -16,5 +18,5 @@ export async function createHandler(c: Context) {
 
   c.status(201)
 
-  return c.json(newSnippet)
+  return c.json<Snippet>(newSnippet)
 }
